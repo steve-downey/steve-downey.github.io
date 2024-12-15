7b7b30072eb0972dc39da1859358e8c725f1e61f
Steve Downey <sdowney@gmail.com>
2024-12-15 18:56:30 -0500

    Add google analytics

--- conf.py
+++ conf.py
@@ -1264,7 +1264,17 @@ FEED_TEASERS = False
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
-# BODY_END = ""
+BODY_END = '''
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VGV27RDN3E"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VGV27RDN3E');
+</script>
+'''
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
